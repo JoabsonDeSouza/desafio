5015522ff39177a5cce2a2cccb57ba1e44daba14
Joabson <joabsondesouza@hotmail.com>
2020-03-23 14:27:37 -0300

    Disable YellowBox

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import store from './store';
 
 import Routes from '~/routes';
 
+console.disableYellowBox = true;
 const App = () => (
   <Provider store={store}>
     <StatusBar barStyle="light-content" />
